ee512ca6f67d95ebbcd9b1477d3d866637daad92
Allain Lalonde <allain.lalonde@gmail.com>
2015-09-18 16:17:19 -0400

    using global.Promise instead of including Promise

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-var Promise = require('bluebird');
+// allow users of this module to define their own Promise implementation
+var Promise = global.Promise;
+if (!Promise) {
+  throw new Error('no global Promise impelementation found, please define one global.Promise = require(\'promise\')');
+}
 
 module.exports = function(val) {
   return Promise.resolve(val);
